81021365b6777d6e54ff56c63d7a82cbd823f231
micheledidonna01 <micheledidonna337@gmail.com>
2025-06-23 15:24:21 +0200

    feat: complete snack 3

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ function somma(a, b) {
 }
 somma(1, 2);
 
-const sommaAnonima = function (a, b){
-    console.log( a + b);
+const sommaAnonima = function (a, b) {
+    console.log(a + b);
 }
 sommaAnonima(3, 4);
 
-const sommaArrow = (a,b)=> console.log(a+b);
+const sommaArrow = (a, b) => console.log(a + b);
 sommaArrow(5, 6);
 
 
@@ -19,3 +19,12 @@ sommaArrow(5, 6);
 
 const numeroQuadrato = (a) => console.log(a * a);
 numeroQuadrato(2);
+
+/* Snack 3 */
+const divisione = (a, b) => a / b;
+const sottrazione = (a, b) => a - b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
+
+console.log(eseguiOperazione(4, 2, divisione));
+console.log(eseguiOperazione(10, 1, sottrazione));

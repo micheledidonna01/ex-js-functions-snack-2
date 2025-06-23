6c1bec973122bdb7d864787fe625aa056d8b3441
micheledidonna01 <micheledidonna337@gmail.com>
2025-06-23 15:15:28 +0200

    fix: snack 1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
 /* Snack 1 */
 
-const a = 1;
-const b = 2;
 
 function somma(a, b) {
     console.log(a + b);
 }
+somma(1, 2);
 
 const sommaAnonima = function (a, b){
     console.log( a + b);
 }
+sommaAnonima(3, 4);
 
 const sommaArrow = (a,b)=> console.log(a+b);
-
-
-
+sommaArrow(5, 6);
